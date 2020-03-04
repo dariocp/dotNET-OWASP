d7acaf5999c091fbd9409a95ff71a2a887bf3058
Darío Cerredelo <dariocp@outlook.com>
2020-03-04 20:41:02 +0100

    Add files via upload
    A1-Injection

---
+++ A1/A1.Server/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using A1.Server.Models;
+using Dapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+
+namespace A1.Server.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(Login login, string returnUrl = "")
+        {
+            string sql = "select Id, Name, Password from [User] where Name='" + login.Username + "' and Password='" + login.Password + "'";
+            User user = new SqliteConnection("Data Source=auth.db").QueryFirstOrDefault<User>(sql);
+
+            // string sql = "select Id, Name, Password from [User] where Name=@Name and Password=@Password";
+            // User user = new SqliteConnection("Data Source=auth.db").QueryFirstOrDefault<User>(sql, new { Name = login.Username, Password = login.Password });
+
+            if (user != null)
+            {
+                var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.Name) };
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+                if (Url.IsLocalUrl(returnUrl)) return Redirect(returnUrl);
+                else return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            ModelState.Remove("Password");
+            return View();
+        }
+        
+        [Authorize]
+        public async Task<ActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ A1/README.md
@@ -0,0 +1,6 @@
+Procedimiento
+
+1. Arrancamos el sitio web Server.
+2. Como usuario anónimo, accedemos a Log in.
+	- Introducimos en el campo Username el siguiente código SQL: ' or 1=1 --
+	- Nos logueamos con el primer usuario que cumple la condición de la consulta.
