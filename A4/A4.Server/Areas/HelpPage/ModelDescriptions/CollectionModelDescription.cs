85e14c800de7fb75931e279279a6bdd966cb0cb8
Darío Cerredelo <dariocp@outlook.com>
2020-03-06 10:42:46 +0100

    Add files via upload
    A4-XML External Entities (XXE)

---
+++ A4/A4.Server/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace A4.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ A4/A4.Server/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace A4.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
